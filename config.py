7fdb1cce9866bccc79e1e87aec2ceac8f450666f
ritesh2512 <36127401+ritesh2512@users.noreply.github.com>
2021-02-24 19:36:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-API_KEY = "a38f4a6a8cb713fe2bebdbf3df331f54"
-PASSWORD = "3182dcd29ff6c3f6f2dd325ba99b4216"
+API_KEY = "****"
+PASSWORD = "****"
 STORE_URL = "mishipaytestdevelopmentemptystore.myshopify.com"
 mishipay_order_list_url = (
     f"https://{API_KEY}:{PASSWORD}@{STORE_URL}/admin/api/2021-01/products.json"
